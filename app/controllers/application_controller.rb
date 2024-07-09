887248dcfb0c5b31b9e8bb9c44d5f25cc3876a48
Vu Thi Ngoc <56816494+ngocvt-0484@users.noreply.github.com>
2024-07-09 08:47:48 +0700

    Chapter 3,4,5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    valid_locales = I18n.available_locales.map(&:to_s)
+    locale = params[:locale]
+
+    I18n.locale = valid_locales.include?(locale) ? locale : I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,9 @@ module RailsTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :vi
+    config.i18n.available_locales = [:en, :vi]
+    # Đường dẫn đến các file dịch
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
   end
 end
